8e0def7c971a897d86d26de4347213acc8722dce
tarymee <tarymee@gmail.com>
2024-07-04 14:18:50 +0800

    innit

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,7 @@
 
 export const WxworksuitePluginInstall: any
 export const jssdk: any
+export const isWxworkSuiteTenant: any
+export const isWxwork: any
+export const isWxworkPc: any
+export const isWxworkApp: any
